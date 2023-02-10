3a2b5a06ff9a708fb62e4845ec117269b1093bbf
yenruogan <zachnagourney@gmail.com>
2023-02-10 10:36:13 -0500

    Added while loop to check user input

--- app.js
+++ app.js
@@ -1,44 +1,49 @@
 // User inputs "rock", "paper", or "scissors"
 const playerSelection = function getPlayersChoice() {
     let playerSelectionNew = prompt("Please input 'rock', 'paper', or 'scissors':")
-    return playerSelectionNew.toLowerCase()
+    while (playerSelectionNew !== "rock" && playerSelectionNew !== "paper" && playerSelectionNew !== 'scissors') {
+        playerSelectionNew = prompt("Try again: rock, paper, or scissors?");
+    }
+    return playerSelectionNew[0].toUpperCase() + playerSelectionNew.slice(1).toLowerCase()
 }
+
 // Computer is randomly assigned "rock", "paper", or "scissors"
 const computerSelection = function getComputerChoice() {
     let computerChoice = Math.random() * 4
     computerChoice = Math.floor(computerChoice)
     if (computerChoice <= 1) {
-        return "rock"
+        return "Rock"
     } else if (computerChoice === 2) {
-        return "paper"
+        return "Paper"
     } else if (computerChoice === 3) {
-        return "scissors"
+        return "Scissors"
     }
 }
 // If user input and computer input are the same it is a tie
+// If user input is "rock" and computer input is "scissors", user wins
+// If user input is "paper" and computer input is "rock", user wins
+// If user input is "scissors" and computer input is "paper", user wins
+// Otherwise computer wins
 function playRound(playerInput, computerInput) {
     if (playerInput === computerInput) {
         return `We have a tie! ${playerInput} and ${computerInput} are the same!`
-    } else if (playerInput === 'rock' && computerInput === 'scissors') {
-        return "You win! Rock beats Paper"
-    } else if (playerInput === 'paper' && computerInput === 'rock') {
+    } else if (playerInput === 'Rock' && computerInput === 'Scissors') {
+        return "You win! Rock beats Scissors"
+    } else if (playerInput === 'Paper' && computerInput === 'Rock') {
         return "You win! Paper beats Rock"
-    } else if (playerInput === 'scissors' && computerInput === 'paper') {
+    } else if (playerInput === 'Scissors' && computerInput === 'Paper') {
         return "You win! Scissors beats Paper"
     } else {
         return `You lose! ${computerInput} beats ${playerInput}`
     }
 }
 
+//Created a function to run the whole game 
 function game() {
     let rounds = prompt("How many rounds would you like to play?")
     for (i = 0; i < rounds; i++) {
         console.log(playRound(playerSelection(), computerSelection()))
     }
 }
-// If user input is "rock" and computer input is "scissors", user wins
-// If user input is "paper" and computer input is "rock", user wins
-// If user input is "scissors" and computer input is "paper", user wins
-// Otherwise computer wins
-// Message appears if user would like to play again
+
 console.log(game());
